ddd5a78ee96422c1780233a5a97e68cdf62fd035
Rodika Aryku <raryku@yahoo.com>
2024-05-09 16:55:21 -0700

    alert added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useEffect, useState} from 'react';
+// import { useCallback } from 'react';
 import './App.css';
 import './index.css';
 import video from './recipes.mp4';
 import image from './salad.png';
 import MyRecipesComponent from './MyRecipesComponent';
 import LoaderPage from './LoaderPage';
+import Swal from 'sweetalert2';
 
 
 
@@ -17,13 +19,93 @@ function App() {
       const [wordSubmitted, setWordSubmitted] = useState(" ");
       const [stateLoader, setStateLoader] = useState(false)
 
+       
+//     const getNewRecipe = useCallback(async () => {
+//       setStateLoader(true)
+//       const responce = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${ MY_ID}&app_key=${MY_KEY}`);
+
+//       if (responce.ok){
+//         setStateLoader(false)
+//         const data = await responce.json();
+//         setMyRecipes(data.hits);
+//       } else {
+//         setStateLoader(false);
+//         alert("type an inggredient")
+//         // Swal.fire("Ingredients entered incorrectly!");
+//       }
+//       // const data = await responce.json();
+//       // setMyRecipes(data.hits);
+//       // setStateLoader(false)
+//     },[wordSubmitted])
+//     useEffect(() =>{
+//       if (wordSubmitted !== '') {
+//         let mySearch = wordSubmitted.split(/[,,;,\n,\r]/);
+//         getNewRecipe(mySearch);
+//       }
+//       // getNewRecipe();
+//       }, [wordSubmitted])
+      
+
+  
+//   const myRecipeSearch = (e) => {
+//   setMySearch(e.target.value);
+// }
+// const finalSearch = (e) => {
+//   e.preventDefault();
+//   setWordSubmitted(mySearch);
+// }
+
+
+    //   const getRecipe = async () => {
+    //     setStateLoader(true)
+    //     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${ MY_ID}&app_key=${MY_KEY}`)
+    //     if(response.ok) {
+    //       setStateLoader(false);
+    //       const data = await response.json();
+    //       setMyRecipes(data.hits);
+    //       setStateLoader(false)
+    //   } 
+    //   else {
+    //     setStateLoader(false);
+    //     alert("Type an ingredient")
+    //     // Swal.fire("Ingredients entered incorrectly!");
+    //   }
+    // }
+    // const myRecipeSearch = (e) => {
+    //     setMySearch(e.target.value)
+    //   }
+    //       const finalSearch = (e) => {
+    //       e.preventDefault();
+    //       setWordSubmitted(mySearch)
+    //    }
+
+    //   useEffect(() => {
+    //     if (wordSubmitted !== '') {
+    //       let ingr = wordSubmitted.split(/[,,;,\n,\r]/);
+    //       getRecipe(ingr);
+    //     }
+    //   }, [wordSubmitted])
+
       useEffect(() => {
         const getRecipe = async () => {
           setStateLoader(true)
-          const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${ MY_ID}&app_key=${MY_KEY}`)
-          const data = await response.json();
-          setMyRecipes(data.hits);
-          setStateLoader(false)
+          const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${ MY_ID}&app_key=${MY_KEY}`);
+
+          if (response.ok) {
+            setStateLoader(false);
+            const data = await response.json();
+            setMyRecipes(data.hits);
+           } 
+        else if (wordSubmitted !== ''){
+          let subm = wordSubmitted.split(/[,,;,\n,\r]/);
+          getRecipe(subm)
+        }
+
+           else{
+              setStateLoader(false);
+              Swal.fire("Ingredients entered incorrectly!");
+            
+          }
         }
         getRecipe();
       }, [wordSubmitted])
@@ -37,11 +119,32 @@ function App() {
          setWordSubmitted(mySearch)
       }
 
+
+      //  useEffect(() => {
+      //   const getRecipe = async () => {
+      //   setStateLoader(true)
+      //   const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${ MY_ID}&app_key=${MY_KEY}`);
+      //   const data = await response.json();
+      //   setMyRecipes(data.hits);
+      //   setStateLoader(false)
+      // }
+      //  getRecipe();
+      //   }, [wordSubmitted])
+
+      //   const myRecipeSearch = (e) => {
+      //   setMySearch(e.target.value)
+      // }
+
+      // const finalSearch = (e) => {
+      //   e.preventDefault();
+      //   setWordSubmitted(mySearch)
+      // }
+
+
     return(
       <div className='App'>
 
         {stateLoader && <LoaderPage/>}
-
 
         <div className='container'>
           <video autoPlay muted loop>
